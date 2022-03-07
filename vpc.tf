70218b0fdd48dcdfbd80053c1231ccf4edb6349e
Mansipande28 <90886981+Mansipande28@users.noreply.github.com>
2022-03-07 10:48:57 +0530

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+terraform {
+  backend "s3" {
+      bucket = "mydemostate"
+      key = "myapp/prod/terraform.tfstate"
+      region = "us-east-1"
+  }
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "BackendDemo"
+    }
+}
